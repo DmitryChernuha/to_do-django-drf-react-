df916b8165d7ebc4cb04897944339e525bae558c
User <chernuhadmitry@gmail.com>
2023-12-08 23:07:49 +0300

    Add api

---
+++ todobackend/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('todos/', views.TodoListCreate.as_view()),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('todos/<int:pk>/complete/', views.TodoToggleComplete.as_view()),
+]
