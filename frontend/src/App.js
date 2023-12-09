37bf27d98e20ddcd724995959afbba2e651cd6e2
User <chernuhadmitry@gmail.com>
2023-12-09 09:56:41 +0300

    Add components and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AddTodo from './components/add-todo';
+import TodosList from './components/todos-list';
+import Login from './components/login';
+import Signup from './components/signup';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <Navbar bg="primary" variant="dark">
+            <div className="container-fluid">
+                <Navbar.Brand>React-bootstrap</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                </Nav>
+            </div>
+        </Navbar>
+    );
 }
 
 export default App;
