d6b25b0d6a038288972365e85ee6d12cf43cb141
User <chernuhadmitry@gmail.com>
2023-12-10 10:39:33 +0300

    Add axios and routes to back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Switch, Route, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {useState} from "react"
 
 import AddTodo from './components/add-todo';
 import TodosList from './components/todos-list';
@@ -14,7 +15,22 @@ import Container from 'react-bootstrap/Navbar';
 
 function App() {
 
-    const user = null
+    //const user = null
+    const [user, setUser] = useState(null)
+    const [token, setToken] = useState(null)
+    const [error, setError] = useState('')
+
+    async function login(user = null) {
+        setUser(user)
+    }
+
+    async function logout() {
+        setUser(null)
+    }
+
+    async function signup(user = null) {
+        setUser(user)
+    }
 
     return (
         <div className="App">
@@ -37,6 +53,36 @@ function App() {
                     </Nav>
                 </div>
             </Navbar>
+            <div className="container mt-4">
+                <Switch>
+                    <Route exact path={["/", "/todos"]} render={(props) =>
+                        <TodosList {...props} token={token}/>
+                    }>
+                    </Route>
+                    <Route path="/todos/create" render={(props) =>
+                        <AddTodo {...props} token={token}/>
+                    }>
+                    </Route>
+                    <Route path="/todos/:id/" render={(props) =>
+                        <AddTodo {...props} token={token}/>
+                    }>
+                    </Route>
+                    <Route path="/login" render={(props) =>
+                        <Login {...props} login={login}/>
+                    }>
+                    </Route>
+                    <Route path="/signup" render={(props) =>
+                        <Signup {...props} signup={signup}/>
+                    }>
+                    </Route>
+                </Switch>
+            </div>
+            <footer className='text-center text-lg-start bg-light text-muted mt-4"'>
+                <div className="text-center p-4">
+
+                <p className="text-reset fw-bold text-decoration-none">© Copyright by Black</p>
+                </div>
+            </footer>
         </div>
     );
 }
